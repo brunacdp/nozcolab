46a01b993f38595f4ba8c64b102fa312c1759c2b
brunacdp <bruna.campos@gmail.com>
2023-10-05 18:56:21 -0300

    header menu ativo

--- script.js
+++ script.js
@@ -19,25 +19,15 @@ function initAnimacaoScroll() {
 }
 initAnimacaoScroll()
 
-/* function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+const linksMenu = document.querySelectorAll('.menu-ul a');
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
-    }
-
-    animaScroll();
-
-    window.addEventListener('scroll', animaScroll);
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add('ativo');
   }
+console.log(href);
 }
-initAnimacaoScroll(); */
+
+linksMenu.forEach(ativarLink);
