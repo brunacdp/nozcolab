0e155bc7908e0b354be4cf9f82d3bea8cd6d0445
brunacdp <brunacamposdev@gmail.com>
2025-04-24 12:13:01 -0300

    correção formulários

--- formulario.js
+++ formulario.js
@@ -2,7 +2,7 @@ const formulario = document.querySelector("form");
 
 function formularioEnviado(resposta) {
   if (resposta.ok) {
-    formulario.innerHTML = "<p class='font2-m' style='border-radius: 8px; background: #FEFFCE;'><span style='color: #317A00;'>Mensagem enviada</span>. Em breve retornaremos seu contato.</p>"
+    formulario.innerHTML = "<p class='font2-m' style='border-radius: 8px; background: #FEFFCE;'><span style='color: #317A00;'>Mensagem enviada</span>. Em breve retornaremos seu contato.</p>";
   } else {
     formulario.innerHTML = "<p class='font2-m' style='border-radius: 8px; background: #FEFFCE;'><span style='color: #E00000;'>Erro no envio</span>. Você pode enviar diretamente para o nosso e-mail em: <span style='font-weight: bold'>contato@nozcolab.com.br</span></p>";
   }
@@ -16,8 +16,8 @@ function enviarFormulario(event) {
 
   const data = new FormData(formulario);
 
-  fetch('./enviar.php', {
-    method: 'POST`',
+  fetch('enviar.php', {
+    method: 'POST',
     body: data,
   }).then(formularioEnviado);
 }
